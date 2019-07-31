dc080772220cdb9c594a1a58e4930629fa1421e2
xvpenghao <xupenghao1996@163.com>
2019-07-31 22:25:13 +0800

    选择排序

--- mySort/select.go
+++ mySort/select.go
@@ -1,1 +1,22 @@
 package mySort
+
+/**
+*@Author:徐鹏豪
+*@Time:2019/7/31 0031
+*@Description:选择排序
+ */
+func SelectSort(arrs []int) {
+	var minIndex int
+	for i := 0; i < len(arrs); i++ {
+		minIndex = i
+		for j := i + 1; j < len(arrs); j++ {
+			if arrs[j] < arrs[minIndex] {
+				minIndex = j
+			}
+		}
+		//如果不一样则交换
+		if minIndex != i {
+			arrs[minIndex], arrs[i] = arrs[i], arrs[minIndex]
+		}
+	}
+}
--- test/sort_test.go
+++ test/sort_test.go
@@ -5,8 +5,23 @@ import (
 	"testing"
 )
 
+//测试堆排序
 func TestHeapSort(t *testing.T) {
 	nums := []int{5, 3, 9, 1, 2, 6}
 	mySort.HeapSort(nums)
+	t.Log("排序后的数组为：", nums)
+}
+
+//测试冒泡排序
+func TestBobbleSort(t *testing.T) {
+	nums := []int{5, 3, 9, 1, 2, 6}
+	mySort.BobbleSort(nums)
+	t.Log("排序后的数组为：", nums)
+}
+
+//测试选择排序
+func TestSelectSort(t *testing.T) {
+	nums := []int{5, 3, 9, 1, 2, 6}
+	mySort.SelectSort(nums)
 	t.Log("排序后的数组为：", nums)
 }
