c1fe71dbcfa1d45e811426781be0bc24d2c8200d
xvpenghao <xupenghao1996@163.com>
2019-08-05 19:05:50 +0800

    快速排序

--- mySort/quick.go
+++ mySort/quick.go
@@ -1,1 +1,44 @@
 package mySort
+
+/**
+*@Author:徐鹏豪
+*@Time:2019/8/5 0005
+*@Description:快速排序
+*/
+func QuickSort(arrs []int, low,high int){
+	if low <high{
+		pivotkey:= partition(arrs,low,high)
+
+		//算左边
+		QuickSort(arrs,low,pivotkey-1)
+		//算右边
+		QuickSort(arrs,pivotkey+1,high)
+	}
+}
+
+//划分，分开
+func partition(arrs []int,low,high int)int{
+	//选择一个作为中枢记录
+	pivotal := arrs[low]
+
+	//以中枢key进行划分，左边小于key的，右边为大于key的
+	for low < high{
+		//先走右边 ,如果不写相等，则high则会处于死循环
+		//而则循环就是看 low 和high在变化的
+		for low <high && arrs[high]>=pivotal{
+			high--
+		}
+		//赋值
+		arrs[low] = arrs[high]
+
+		//看中枢左边的
+		for low <high && arrs[low] <pivotal{
+			low++
+		}
+		arrs[high] = arrs[low]
+	}
+
+	//算出中间的节点
+	arrs[low] = pivotal
+	return low
+}
--- test/sort_test.go
+++ test/sort_test.go
@@ -32,3 +32,9 @@ func TestInsertSort(t *testing.T) {
 	mySort.InsertSort(nums)
 	t.Log("排序后的数组为：", nums)
 }
+
+func TestQuickSort(t *testing.T) {
+	nums := []int{5, 3, 9, 1, 2, 6}
+	mySort.QuickSort(nums,0,len(nums)-1)
+	t.Log("排序后的数组为：", nums)
+}
