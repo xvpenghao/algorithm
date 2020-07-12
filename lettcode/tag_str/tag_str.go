e40c11fd18cea8884b9fa4e5a63faafd617df2d9
xvpenghao <xupenghao1996@163.com>
2020-07-12 11:29:23 +0800

    offer 03-数组中重复的数字

---
+++ lettcode/tag_str/tag_str.go
@@ -0,0 +1,9 @@
+package tag_str
+
+
+//05-最长回文字符串
+//https://leetcode-cn.com/problems/longest-palindromic-substring/
+func longestPalindrome(s string) string {
+
+	return ""
+}
---
+++ offer/offer.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 3, 1, 0, 2, 5, 3}
+	fmt.Println(findRepeatNumber2(nums))
+}
+
+
+func findRepeatNumber3(nums []int) int {
+	h := make([]bool,len(nums))
+	for _, val := range nums{
+		if h[val] {
+			return val
+		}
+		h[val] = true
+	}
+	return 0
+}
+
+
+//数组中的数字的在0~len(num)-1 范围内
+//所谓的冲突就是 多个值可能共用了下标
+func findRepeatNumber2(nums []int) int {
+	for index, val := range nums{
+		//则不用交换
+		if index == val {
+			continue
+		}
+		//说明冲突要交换
+		if val == nums[val] {
+			return val
+		}
+		nums[index],nums[val] = nums[val],nums[index]
+	}
+	return 0
+}
+
+//剑指 Offer 03. 数组中重复的数字
+//https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+func findRepeatNumber(nums []int) int {
+	numMap := make(map[int]bool)
+	for _, n := range nums{
+		if _,ok := numMap[n];ok{
+			return n
+		}
+		numMap[n] = true
+	}
+	return 0
+}
+
+
+
